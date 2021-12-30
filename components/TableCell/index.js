32827b5db418330999ba8b88c42c2bf4e3f26775
kodcanlisi <developerindiee@gmail.com>
2021-12-30 22:07:04 +0300

    ✏️handle total width with sending an array

--- components/TableCell/index.js
+++ components/TableCell/index.js
@@ -5,13 +5,17 @@ import styles from '../../styles';
 import Utils from '../../utils';
 
 const TableCell = (props) => {
-    const { text, align, totalElements, width, totalWidth, totalElementsThatHaveWidth } = props;
+    const { text, align, __totalElements, __elementsWidthArray, width } = props;
+
+    const totalWidth = Utils.sumOfArray(__elementsWidthArray);
+    const totalElementsThatHaveWidth = __elementsWidthArray.filter(width => width > 0).length;
+
     let style = { width: width + "%" };
 
     if(width){
         style.width = width + "%";
     } else{
-        style.width = Utils.calculateWidth(totalWidth, totalElements-totalElementsThatHaveWidth);
+        style.width = Utils.calculateWidth(totalWidth, __totalElements-totalElementsThatHaveWidth);
     }
     if (align === 'center' || align === 'right' || align === 'left') {
         style.textAlign = align;
@@ -27,6 +31,13 @@ const TableCell = (props) => {
 
 TableCell.propTypes = {
     /**
+     * The width for this table cell.
+     * If not set, the width will be calculated based on the total width of the row.
+     * @default 0
+     */
+    width: PropTypes.number,
+
+    /**
      * The text to display in the header cell
      */
     text: PropTypes.string.isRequired,
@@ -35,6 +46,11 @@ TableCell.propTypes = {
      * The style to apply to the header cell
      */
     align: PropTypes.oneOf(['left', 'right', 'center']),
+}
+
+TableCell.defaultProps = {
+    width: 0,
+    align: 'left',
 }
 
 export default TableCell;
--- components/TableRow/index.js
+++ components/TableRow/index.js
@@ -9,17 +9,21 @@ const TableRow = (props) => {
     const { width, disableMargin } = props;
     let style = { width: width + "%" };
 
+    if(width){
+        style.width = width + "%";
+    }else{
+        style.width = "auto";
+    }
+
     if(disableMargin){
         style.margin = 0;
     }
 
     let totalWidth = React.Children.map(props.children, child => child.props.width || 0);
-    let totalElementsThatHaveWidth = totalWidth.filter(width => width > 0).length;
-    totalWidth = totalWidth.reduce((a, b) => a + b, 0);
 
     const renderChildren = React.Children.map(props.children, (child) => {
         if (child.type === TableCell) {
-            return React.cloneElement(child, { totalElements: React.Children.count(props.children), totalWidth: totalWidth, totalElementsThatHaveWidth });
+            return React.cloneElement(child, { __totalElements: React.Children.count(props.children), __elementsWidthArray: totalWidth,  });
         }
         if(child.type === TableRow) {
             return React.cloneElement(child);
@@ -33,5 +37,24 @@ const TableRow = (props) => {
         </>
     )
 }
+
+TableRow.propTypes = {
+    /**
+     * The with of the table row
+     * @default 0
+     */
+    width: PropTypes.number,
+    /**
+     * Disable margin for the row
+     * @default false
+     */
+    disableMargin: PropTypes.bool,
+}
+
+TableRow.defaultProps = {
+    width: 0,
+    disableMargin: false,
+}
+
 
 export default TableRow;
